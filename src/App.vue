3ba10e1236026538cc2fd19a2ad8ed14dc198dc8
Kabirul Islam Mondal <kabirul05@gmail.com>
2022-05-17 19:30:37 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-info">
+      
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/home" class="nav-link">
+            <font-awesome-icon icon="home" /> Home
+          </router-link>
+        </li>       
+      </div>
+
+      <div v-if="!currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link">
+            <font-awesome-icon icon="user-plus" /> Sign Up
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">
+            <font-awesome-icon icon="sign-in-alt" /> Login
+          </router-link>
+        </li>
+      </div>
+
+      <div v-if="currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">
+            <font-awesome-icon icon="user" />
+            {{ currentUser.username }}
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click.prevent="logOut">
+            <font-awesome-icon icon="sign-out-alt" /> LogOut
+          </a>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view />
+    </div>
+		<Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer.vue';
+
+export default {
+   components: {    
+       Footer	
+   },  
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    }  
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
